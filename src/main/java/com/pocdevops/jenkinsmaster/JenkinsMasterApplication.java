d14c212273fcac0979e02340a1484e6b0a31767b
joaomarcosfelixds <joaomarcosfelixds@gmail.com>
2021-06-16 10:49:55 -0300

    init project

---
+++ src/main/java/com/pocdevops/jenkinsmaster/JenkinsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.pocdevops.jenkinsmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocdevops/jenkinsmaster/JenkinsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocdevops.jenkinsmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
